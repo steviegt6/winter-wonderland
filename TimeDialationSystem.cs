3983d91c713c440b23f71138afa4d56a34187cb9
Tomat <xxlennygamerxx@gmail.com>
2022-11-28 21:14:03 -0800

    time dialation

---
+++ TimeDialationSystem.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WinterWonderland;
+
+public class TimeDialationSystem : ModSystem
+{
+    public override void ModifyTimeRate(ref double timeRate, ref double tileUpdateRate, ref double eventUpdateRate) {
+        base.ModifyTimeRate(ref timeRate, ref tileUpdateRate, ref eventUpdateRate);
+
+        if (Main.time > 32400d) timeRate = 2d;
+        if (Main.time > 54000d) timeRate = 0.5d;
+    }
+}
