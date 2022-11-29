777753198ecd5a6d62670eb11c33ae908de9c326
Tomat <xxlennygamerxx@gmail.com>
2022-11-28 21:01:27 -0800

    christmas

---
+++ ChristmasToggleSystem.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WinterWonderland;
+
+public class ChristmasToggleSystem : ModSystem
+{
+    public override void PreUpdateWorld() {
+        base.PreUpdateWorld();
+
+        Main.xMas = true;
+        Main.forceXMasForToday = true;
+    }
+}
