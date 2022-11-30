825dffe577aac85ca555215bac02ea6445ae6807
Tomat <xxlennygamerxx@gmail.com>
2022-11-29 22:37:14 -0800

    brbrbrb

--- WorldStateModifier.cs
+++ WorldStateModifier.cs
@@ -9,9 +9,14 @@ public class WorldStateModifier : ModSystem
     public override void PreUpdateWorld() {
         base.PreUpdateWorld();
 
-        Main.xMas = true;
-        Main.forceXMasForToday = true;
-
         Filters.Scene["Sandstorm"].Active = false;
     }
+
+    public override void PostUpdateWorld() {
+        base.PostUpdateWorld();
+        
+        Main.xMas = true;
+    }
+    
+    
 }
